326d7601aa18198ad97c56d5ba0b55f1de63af13
Stalfork <felipebep2011@gmail.com>
2022-08-07 22:46:37 -0500

    Modify getComputerChoice function to be shorter and add game function to play 5 matches and alert the winner

--- JS.js
+++ JS.js
@@ -1,39 +1,64 @@
-    const options = [
-        "Rock",
-        "Paper",
-        "Scissors"
-    ];
-
+const options = ["ROCK", "PAPER", "SCISSORS"];
 
 function getComputerChoice() {
-    let choice = options[Math.floor(Math.random() * options.length)];
-    return choice;
-};
-
-function playRound(playerSelection, computerSelection) {
-
-    playerSelection = playerSelection.toUpperCase();
-    
-    computerSelection = getComputerChoice().toUpperCase();
-    
-    if (playerSelection === "ROCK" && computerSelection === "PAPER") {
-        return "You lose! Paper beats Rock";
-    } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
-        return "You win! Rock beats Scissors";
-    } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
-        return "You win! Paper beats Rock";
-    } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
-        return "You lose! Scissors beat paper";
-    } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
-        return "You lose! Rock beats Scissors";
-    } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
-        return "You win! Scissors beat paper"
-    } else {
-        return "It's a tie, try again";
-    }
-    
+  return options[Math.floor(Math.random() * options.length)]; // pick a random option 
 }
-const playerSelection = "Rock";
-const computerSelection = getComputerChoice();
 
-console.log(playRound(playerSelection, computerSelection))
+function playRound() { 
+  const playerSelection = prompt("Insert Rock Paper or Scissors").toUpperCase();
+  const computerSelection = getComputerChoice().toUpperCase();
+
+  if (options.includes(playerSelection)) {
+    if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+      return "lose";
+    } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+      return "win";
+    } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+      return "win";
+    } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+      return "lose";
+    } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+      return "lose";
+    } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+      return "win";
+    } else {
+      return "tie";
+    }
+  }
+
+  return false;
+}
+
+let userScore = 0;
+let computerScore = 0;
+
+function game() { 
+  for (let i = 0; i < 5; i++) {
+    const option = playRound();
+
+    if (option === false) {
+      alert("Invalid option, try again");
+    } else {
+      if (option === "win") {
+        userScore++;
+        alert("You win this round, your score is " + userScore + ", the PC score is " + computerScore);
+      } else if (option === "lose") {
+        computerScore++;
+        alert("You lose this round, your score is " + userScore + ", the PC score is " + computerScore);
+      } else {
+        i--; // <<< This makes so if there's a tie, it won't count as an attempt //
+        alert("It's a tie, try again, your score is " + userScore + ", the PC score is " + computerScore);
+      }
+    
+    }
+    const winner = (userScore > computerScore) ? "You Are a Winner!":"You Lost Against the Computer :(";
+    if (i === 4) {
+      alert(winner);
+    }
+  }
+}
+
+game();
+
+
+
